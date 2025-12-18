e2a9047bc22747a0ff103ee13187c65352c6a413
ranjith1807 <v.ranjith9095490240@gmail.com>
2025-12-18 17:39:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function validEmail(str) {
   //your JS code here.
+	let email=/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+	if(email.test(str)){
+		return 'true';
+	}
+	else{
+		return 'false';
+	}
+	
 }
 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
+
+
